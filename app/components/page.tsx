744eb3e2802d09b96e1093946eb6d597532d30f1
Muhammad Shoaib <136969366+shoby056@users.noreply.github.com>
2025-05-26 11:43:13 +0500

    Update page.tsx

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,94 +1,83 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
-
 
 interface Rates {
   BTC?: number;
+  ETH?: number;
   BNB?: number;
   USDT?: number;
-  ETH?: number;
   DOGE?: number;
   ADA?: number;
 }
 
-interface DataType {
-  rates: Rates;
-}
-
-
 export default function CryptoRates() {
-  // const API_KEY = "3251945b5f95ea3a9162dfb71217145c";
-  const API_KEY = "5068db716bc1b9f4fa6963de4c5d4269";
-
-  const [data, setData] = useState<DataType | null> (null);
+  const [rates, setRates] = useState<Rates | null>(null);
 
   useEffect(() => {
-    fetch(`http://api.coinlayer.com/live?access_key=${API_KEY}`)
-      .then((response) => response.json())
-      .then((jsonConverted) => {
-        console.log("JSON Converted Data : ", jsonConverted);
-        setData(jsonConverted);
+    fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,tether,cardano,dogecoin&vs_currencies=usd"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setRates({
+          BTC: data.bitcoin.usd,
+          ETH: data.ethereum.usd,
+          BNB: data.binancecoin.usd,
+          USDT: data.tether.usd,
+          DOGE: data.dogecoin.usd,
+          ADA: data.cardano.usd,
+        });
       });
   }, []);
-
-  // const data = await fetch(`http://api.coinlayer.com/live?access_key=${API_KEY}`) 
-  // const fetched_data = await data.json()
-
-  // console.log(fetched_data)
 
   return (
     <div>
       <nav>
-        <h1 className="main" >Crypto Currency Rates</h1>
+        <h1 className="main">Crypto Currency Rates</h1>
       </nav>
 
       <div className="container">
         <div className="shan">
-          <h1 className="here">Here are crypto rates</h1></div>
+          <h1 className="here">Here are crypto rates</h1>
+        </div>
         <br />
         <div className="buttons">
           <table>
             <thead>
               <tr>
                 <th>Currency</th>
-                <th>Rate</th>
+                <th>Rate (USD)</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>BTC</td>
-                <td>{data?.rates?.BTC}</td>
-              </tr>
-              <tr>
-                <td>BNB</td>
-                <td>{data?.rates?.BNB}</td>
-              </tr>
-              <tr>
-                <td>USDT</td>
-                <td>{data?.rates?.USDT}</td>
+                <td>{rates?.BTC}</td>
               </tr>
               <tr>
                 <td>ETH</td>
-                <td>{data?.rates?.ETH}</td>
+                <td>{rates?.ETH}</td>
+              </tr>
+              <tr>
+                <td>BNB</td>
+                <td>{rates?.BNB}</td>
+              </tr>
+              <tr>
+                <td>USDT</td>
+                <td>{rates?.USDT}</td>
               </tr>
               <tr>
                 <td>DOGE</td>
-                <td>{data?.rates?.DOGE}</td>
+                <td>{rates?.DOGE}</td>
               </tr>
               <tr>
                 <td>ADA</td>
-                <td>{data?.rates?.ADA}</td>
+                <td>{rates?.ADA}</td>
               </tr>
             </tbody>
           </table>
-
         </div>
-
-
-
-
       </div>
-
     </div>
   );
 }
